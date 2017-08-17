76fa694b78feed03685f07e7fc6d041b490ea900
Watson <w@vulk.co>
2017-08-17 23:30:25 +0000

    remove lesson 3

--- lesson3/pusher_lite_demo/app/controllers/events_controller.rb
+++
@@ -1,9 +0,0 @@
-class EventsController < ApplicationController
-  def create
-    SendEventsJob.perform_later(event_params)
-  end
-
-  def event_params
-    params.require(:pusher_event).permit(:name, :message, :broadcast)
-  end
-end
--- lesson3/pusher_lite_demo/config/initializers/pusher_lite.rb
+++
@@ -1,8 +0,0 @@
-module PusherLite
-  def self.uri
-    url    = Rails.application.secrets.pusher_url
-
-    uri = URI::encode("http://#{url}/events")
-    URI.parse(uri)
-  end
-end
